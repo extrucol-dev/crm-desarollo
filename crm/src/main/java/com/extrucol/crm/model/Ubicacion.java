02b32560c59aaa8f60f07ee1b14195483522e6ff
extrucol-dev <desarrollo@extrucol.com>
2026-03-20 08:00:56 -0500

    feat: :sparkles: add Ubicacion and Usuario entities, configure application properties, and initialize front-end structure

--- crm/src/main/java/com/extrucol/crm/model/Ubicacion.java
+++ crm/src/main/java/com/extrucol/crm/model/Ubicacion.java
@@ -1,4 +1,31 @@
 package com.extrucol.crm.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "CRM_UBICACION")
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
 public class Ubicacion {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_ubicacion")
+    Long id;
+
+    @Column(nullable = false)
+    BigDecimal latitud;
+
+    @Column(nullable = false)
+    BigDecimal longitud;
+
+    @JoinColumn
+    @OneToOne(fetch = FetchType.LAZY)
+    Actividad actividad;
 }
--- crm/src/main/java/com/extrucol/crm/model/Usuario.java
+++ crm/src/main/java/com/extrucol/crm/model/Usuario.java
@@ -1,4 +1,39 @@
 package com.extrucol.crm.model;
 
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "CRM_UBICACION")
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
 public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_usuario")
+    Long id;
+
+    @Column(nullable = false)
+    String nombre;
+
+    @Column(nullable = false)
+    String email;
+
+    @Column(nullable = false)
+    String password;
+
+    @Column(nullable = false)
+    String rol;
+
+    @Column(nullable = false)
+    Integer activo;
+
+    @Column(nullable = false)
+    LocalDate fechaCreacion;
 }
